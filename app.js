9524e5b97d634dc001d4c5b490d838aaa6d2ebff
Chirag221020 <chiragchouhan2220@gmail.com>
2024-06-28 02:26:03 +0530

    code update

--- app.js
+++ app.js
@@ -46,6 +46,5 @@ const scrollUp = () =>{
   //  When the scroll is higher than 350 viewport height,  add the top navigate button.
   this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');}
   window.addEventListener('scroll', scrollUp)
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
