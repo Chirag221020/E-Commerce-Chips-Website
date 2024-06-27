1168c23abd1cc7e0bf955ae74aba88d03aa50945
Chirag221020 <chiragchouhan2220@gmail.com>
2024-06-28 02:24:40 +0530

    final commit

--- app.js
+++ app.js
@@ -41,7 +41,11 @@ const swiperFav = new Swiper('.fav-swiper', {
   centeredSlides: 'auto',});
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () =>{
+  const scrollUp = document.getElementById('scroll-up')
+  //  When the scroll is higher than 350 viewport height,  add the top navigate button.
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');}
+  window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
